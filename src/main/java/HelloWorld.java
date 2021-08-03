837b0147dc33e009b8455f16aeec0ddf9af271bc
TRanthienTam <TRanthienTam@gmail.com>
2021-08-03 17:31:39 +0000

    HCM

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello Ho Chi Minh City");
+		get("/hello", (req, res) -> "Hello Ho Chi Minh City 2021");
 	}
 }
